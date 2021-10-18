07288e5ce771f9da3257e0a105e1c8f25791a120
Manipreet Ietharaju <92304285+Mani2010@users.noreply.github.com>
2021-10-18 13:53:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+function Calculate()
+{
+var result=
+Number(document.getElementById("firstno").value)+
+Number(document.getElementById("secondno").value);
+window.alert (result);
+}
+
+function Calculate2()
+{
+var result=
+Number(document.getElementById("firstno").value)*
+Number(document.getElementById("secondno").value);
+window.alert (result);
+}
+
+function Calculate3()
+{
+var result=
+Number(document.getElementById("firstno").value)-
+Number(document.getElementById("secondno").value);
+window.alert (result);
+}
+
+
+function Calculate4()
+{
+var result=
+Number(document.getElementById("firstno").value)/
+Number(document.getElementById("secondno").value);
+window.alert (result);
+}
